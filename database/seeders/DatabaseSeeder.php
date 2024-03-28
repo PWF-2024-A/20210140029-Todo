9272222da1526b1e16481de8dc38196141564cd9
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2024-03-28 09:16:27 +0700

    migration, database seeder, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Todo;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +17,31 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // \App\Models\
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'is_admin' => true
+        ]);
+
+        User::factory()->create([
+            'name' => 'Alif nur rachman',
+            'email' => 'alifnurrachman@gmail.com',
+            //'is_admin' => false
+        ]);
+
+        /*User::create(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkj00r0Q5byMi.Ye4oKoEa3Ro911C/.og/at2.uheWG/igi', //password
+                'remember_token' => Str::random(10),
+                'is_admin' => true
+            ]
+        );*/
+
+        User::factory(100)->create();
+        Todo::factory(500)->create();
     }
 }
